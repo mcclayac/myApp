6940919b9bc838d058ab533f3797b6daed2042e8
mcclayac <anthony.mcclay@me.com>
2019-03-08 04:01:10 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+"""
+        
+   created: mcclayac
+   Company Name : BigMAN Software
+   MyName: Tony McClay
+   date: 2019-03-07
+   day of month: 07
+   
+   Project Name: myApp
+   filename:  app.py
+   package name: 
+   IDE: PyCharm
+   
+   
+"""
+
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
